6ecce3eed8443a96c3bde1a28905820d265ac972
richard <richardyantas5@gmail.com>
2019-02-03 15:38:10 -0500

    Triangle example

---
+++ include/utils.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include "../include/utils.h"
+
+using namespace std;
+
+class A
+{
+    public:
+        void print();
+};
+
---
+++ src/utils.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void print()
+{
+    cout << "A was created" << endl;
+}
+
+//void A::print(){}
